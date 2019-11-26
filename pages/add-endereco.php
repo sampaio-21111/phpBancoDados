d88454752d22f8bcde383c6ecf2da57ec19f5f15
Técnico em Informática - 2019.1 <169121742019.1@senacrjedu.local>
2019-11-26 19:34:48 -0300

    oi

---
+++ pages/add-endereco.php
@@ -0,0 +1,40 @@
+<div class="container">
+    <form method="POST">
+        <div class="form-group">
+            <label for="cep">CEP</label>
+            <input type="text" class="form-control" id="cep" name="cep" onblur="pesquisacep(this.value)" maxlength="9" require>
+        </div>
+        <div class="form-group">
+            <label for="logradouro">Endereço</label>
+            <input type="text" class="form-control" id="logradouro" name="logradouro" require>
+        </div>
+        <div class="form-group">
+            <label for="bairro">Bairro</label>
+            <input type="text" class="form-control" id="bairro" name="bairro" require>
+        </div>
+        <div class="form-group">
+            <label for="cidade">Cidade</label>
+            <input type="text" class="form-control" id="cidade" name="cidade" require>
+        </div>
+
+        <div class="form-group">
+            <label for="uf">Estado</label>
+            <input type="text" class="form-control" id="uf" name="uf" maxlength="2" require>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Salvar</button>
+        <button type="reset" class="btn btn-primary">Cancelar</button>
+    </form>
+</div>
+<script src="js/viacep.js"></script>
+<?php
+if (!empty($_POST)) {
+    require_once("./model/endereco.php");
+    $endereco = new Endereco;
+    $endereco->cep = $_POST['cep'];
+    $endereco->logradouro = $_POST['logradouro'];
+    $endereco->cidade = $_POST['cidade'];
+    $endereco->bairro = $_POST['bairro'];
+    $endereco->uf = $_POST['uf'];
+    $endereco->add();
+}
